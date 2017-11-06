565f2c537380d195a2e8e394170f2dad72ac1f51
wisnuc-imac <wisnuc-imac@wisnuc-imacdeiMac.local>
2017-11-06 13:57:18 +0800

    download

---
+++ WisnucBox/Modules/Download/CSFileDownloadManager.h
@@ -0,0 +1,31 @@
+//
+//  CSFileDownloadManager.h
+//  WisnucBox
+//
+//  Created by wisnuc-imac on 2017/11/6.
+//  Copyright © 2017年 JackYang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSFileDownloadManager : NSObject
+
+/** 下载进度条 */
+@property (strong, nonatomic)  UIProgressView *progressView;
+/** 下载进度条Label */
+@property (strong, nonatomic)  UILabel *progressLabel;
+
+/** AFNetworking断点下载（支持离线）需用到的属性 **********/
+/** 文件的总长度 */
+@property (nonatomic, assign) NSInteger fileLength;
+/** 当前下载长度 */
+@property (nonatomic, assign) NSInteger currentLength;
+/** 文件句柄对象 */
+@property (nonatomic, strong) NSFileHandle *fileHandle;
+
+/** 下载任务 */
+@property (nonatomic, strong) NSURLSessionDataTask *downloadTask;
+/* AFURLSessionManager */
+@property (nonatomic, strong) AFURLSessionManager *manager;
+
+@end
---
+++ WisnucBox/Modules/Download/Controller/LocalDownloadViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocalDownloadViewController.h
+//  WisnucBox
+//
+//  Created by wisnuc-imac on 2017/11/6.
+//  Copyright © 2017年 JackYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocalDownloadViewController : UIViewController
+
+@end
