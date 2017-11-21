f9552d663c0e39b7aa6b78e087d3007b58b7a888
charlessnow <1126104236@qq.com>
2017-11-21 14:13:42 +0800

    share

---
+++ WisnucBox/Modules/Files/Controller/FilesShareViewController.m
@@ -0,0 +1,122 @@
+//
+//  FilesShareViewController.m
+//  WisnucBox
+//
+//  Created by wisnuc-imac on 2017/11/21.
+//  Copyright © 2017年 JackYang. All rights reserved.
+//
+
+#import "FilesShareViewController.h"
+#import "FilesNextViewController.h"
+#import "FLFilesCell.h"
+
+@interface FilesShareViewController ()
+<
+UITableViewDelegate,
+UITableViewDataSource
+>
+
+@property (strong, nonatomic) UITableView *tableView;
+@end
+
+@implementation FilesShareViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self initView];
+    [self loadData];
+    // Do any additional setup after loading the view.
+}
+
+- (void)initView{
+    [self.view addSubview:self.tableView];
+//    [self initMjRefresh];
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+}
+
+
+- (void)initMjRefresh{
+    __weak __typeof(self) weakSelf = self;
+    
+    self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [weakSelf loadData];
+    }];
+    self.tableView.mj_header.ignoredScrollViewContentInsetTop = KDefaultOffset;
+    [self.tableView.mj_header beginRefreshing];
+}
+
+
+- (void)loadData{
+    [self.tableView reloadData];
+//    [self.tableView.mj_header endRefreshing];
+}
+
+- (void)setDataSouceArrayWith:(NSMutableArray *)array{
+    [self.dataSouceArray addObjectsFromArray:array];
+    [self.tableView reloadData];
+}
+
+#pragma  TableView DataSource
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 64;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    FLFilesCell *cell  = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([FLFilesCell class])];
+    if (nil == cell) {
+        cell= (FLFilesCell *)[[[NSBundle  mainBundle] loadNibNamed:NSStringFromClass([FLFilesCell class]) owner:self options:nil]  lastObject];
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    NSLog(@"%@",self.dataSouceArray);
+    ShareFilesModel *model = self.dataSouceArray[indexPath.row];
+    cell.nameLabel.text = model.label;
+    cell.downBtn.hidden = YES;
+    cell.f_ImageView.image = [UIImage imageNamed:@"share"];
+
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataSouceArray.count;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+//    FirstFilesModel *model = _dataSouceArray[indexPath.row];
+//    if (model.type == WBFilesFirstDirectoryMyFiles) {
+//        FilesNextViewController *filesVC = [[FilesNextViewController alloc]init];
+//        filesVC.name = model.name;
+//        filesVC.parentUUID = WB_UserService.currentUser.userHome;
+//        [self.navigationController pushViewController:filesVC animated:YES];
+//    }else{
+//        FilesViewController * filesVC = [[FilesViewController alloc]init];
+//        [self.navigationController pushViewController:filesVC animated:YES];
+//    }
+}
+
+
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, __kWidth, __kHeight) style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.contentInset = UIEdgeInsetsMake(KDefaultOffset, 0, 0, 0);
+    }
+    return _tableView;
+}
+
+- (NSMutableArray *)dataSouceArray{
+    if (!_dataSouceArray) {
+        _dataSouceArray = [NSMutableArray arrayWithCapacity:0];
+    }
+    return _dataSouceArray;
+}
+
+@end
+
+@implementation ShareFilesModel
+
+@end
--- WisnucBox/Modules/Files/Controller/FirstFilesViewController.h
+++ WisnucBox/Modules/Files/Controller/FirstFilesViewController.h
@@ -6,8 +6,23 @@
 //  Copyright © 2017年 JackYang. All rights reserved.
 //
 
+
+
 #import "FMBaseFirstVC.h"
+@class FirstFilesModel;
+
+typedef NS_ENUM(NSInteger,WBFilesFirstDirectoryType) {
+    WBFilesFirstDirectoryMyFiles,//默认从0开始
+    WBFilesFirstDirectoryShare,
+};
 
 @interface FirstFilesViewController : FMBaseFirstVC
+
+@end
+
+@interface FirstFilesModel : NSObject
+
+@property (nonatomic,strong) NSString *name;
+@property (nonatomic) WBFilesFirstDirectoryType type;
 
 @end
